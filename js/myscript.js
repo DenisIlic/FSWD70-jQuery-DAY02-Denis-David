7fc9bd2ab880689a8d974c8810f9b132c784e61f
Denis Ilic <ilic.denis@gmx.at>
2019-06-04 10:43:54 +0200

    shop

---
+++ js/myscript.js
@@ -0,0 +1,33 @@
+// Basic 1
+
+// $(".Image").click( function() {
+
+// 	alert("You clicked on the Christmas bell");
+// })
+
+// $(".Image1").on("click", function() {
+
+// 	alert("You clicked on Santa");
+
+// })
+
+// $(".Image2").on("click", function() {
+
+// 	alert("You clicked on the White Gift!");
+
+// })
+
+
+
+
+// Basic 2
+
+// $(".santa").on("click", function() {
+
+// 	$(".santa").attr("src", "img/santa.png").hide("slow");
+
+// })
+
+
+
+ 
